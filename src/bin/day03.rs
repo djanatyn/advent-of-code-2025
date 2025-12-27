a2f8b135be11a64d1c9a8f9a335c9087c64ac4ce
Jonathan Strickland <djanatyn@gmail.com>
2025-12-26 21:14:07 -0500

    day3 part 2 wip

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,12 +1,12 @@
 fn solve_part_one(input: &str) -> i64 {
-    input.lines().map(row_largest_joltage).sum()
+    input.lines().map(row_largest_joltage_part_one).sum()
 }
 
 fn solve_part_two(input: &str) -> i64 {
-    todo!()
+    input.lines().map(row_largest_joltage_part_two).sum()
 }
 
-fn row_largest_joltage(input: &str) -> i64 {
+fn row_largest_joltage_part_one(input: &str) -> i64 {
     // highest starting digit is 9
     let mut joltage: i64 = 9;
     loop {
@@ -28,6 +28,34 @@ fn row_largest_joltage(input: &str) -> i64 {
     }
 }
 
+fn row_largest_joltage_part_two(input: &str) -> i64 {
+    // highest starting digit is 9
+    let mut joltage: i64 = 9;
+    let mut digits = String::new();
+    let mut remaining_digits = 12;
+    let mut tokens = input.to_string();
+    loop {
+        dbg!((&input, &digits, &remaining_digits, &tokens, &joltage));
+        let pattern = joltage.to_string();
+        if let Some((_, rest)) = tokens.split_once(&pattern)
+            && (remaining_digits == 1 || (!rest.is_empty()))
+            && (remaining_digits == 1 || (rest.len() >= remaining_digits))
+        {
+            digits.push_str(&joltage.to_string());
+            remaining_digits -= 1;
+            joltage = 9; // reset joltage
+            tokens = rest.to_string(); // only use rest of tokens
+            if remaining_digits == 0 {
+                return digits.parse::<i64>().unwrap();
+            }
+        } else if joltage <= 0 {
+            panic!("failed")
+        } else {
+            joltage -= 1;
+        }
+    }
+}
+
 fn main() {
     let input = std::fs::read_to_string("inputs/input03.txt").unwrap();
     println!("part 1: {}", solve_part_one(&input));
@@ -41,15 +69,32 @@ mod tests {
     #[test]
     fn test_example_part_one() {
         let input = std::fs::read_to_string("inputs/example03.txt").unwrap();
-        assert_eq!(row_largest_joltage("987654321111111"), 98);
-        assert_eq!(row_largest_joltage("811111111111119"), 89);
-        assert_eq!(row_largest_joltage("234234234234278"), 78);
-        assert_eq!(row_largest_joltage("818181911112111"), 92);
+        assert_eq!(row_largest_joltage_part_one("987654321111111"), 98);
+        assert_eq!(row_largest_joltage_part_one("811111111111119"), 89);
+        assert_eq!(row_largest_joltage_part_one("234234234234278"), 78);
+        assert_eq!(row_largest_joltage_part_one("818181911112111"), 92);
         assert_eq!(solve_part_one(&input), 357);
     }
 
-    // #[test]
-    // fn test_example_part_two() {
-    //     todo!()
-    // }
+    #[test]
+    fn test_example_part_two() {
+        let input = std::fs::read_to_string("inputs/example03.txt").unwrap();
+        assert_eq!(
+            row_largest_joltage_part_two("987654321111111"),
+            987654321111
+        );
+        assert_eq!(
+            row_largest_joltage_part_two("811111111111119"),
+            811111111119
+        );
+        assert_eq!(
+            row_largest_joltage_part_two("234234234234278"),
+            434234234278
+        );
+        assert_eq!(
+            row_largest_joltage_part_two("818181911112111"),
+            888911112111
+        );
+        assert_eq!(solve_part_one(&input), 357);
+    }
 }
